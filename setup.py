a7732d9320853add261e682d210eeb5c7a61b05c
Milosz Wasilewski <milosz.wasilewski@linaro.org>
2019-07-16 12:03:16 +0100

    New release: 1.9
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@linaro.org>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='squad-linaro-plugins',
-    version='1.8',
+    version='1.9',
     author='Milosz Wasilewski',
     author_email='milosz.wasilewski@linaro.org',
     url='https://github.com/linaro/squadplugins',
