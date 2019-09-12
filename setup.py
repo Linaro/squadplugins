c33c6523bdf0b12e46b58242cfed95f0b2a5aae2
Charles <18oliveira.charles@gmail.com>
2019-09-12 17:24:39 -0300

    increase squadplugins version

--- setup.py
+++ setup.py
@@ -2,23 +2,24 @@ from setuptools import setup
 
 setup(
     name='squad-linaro-plugins',
-    version='1.9',
+    version='1.10',
     author='Milosz Wasilewski',
     author_email='milosz.wasilewski@linaro.org',
     url='https://github.com/linaro/squadplugins',
-    packages=['tradefed', 'ltp'],
+    packages=['tradefed', 'ltp', 'kernel_issues'],
     entry_points={
         'squad_plugins': [
             'tradefed=tradefed:Tradefed',
             'ltp=ltp:LtpLogs',
+            'kernel_issues=kernel_issues:KernelIssues',
         ]
     },
     license='AGPLv3+',
     description="SQUAD plugins collection",
     long_description="""
     SQUAD plugins that are compatible with Linaro's test-definitions.
-    The package contains plugin for parsing CTS/VTS results (tradefed)
-    and LTP results (ltp).
+    The package contains plugin for parsing CTS/VTS results (tradefed),
+    LTP results (ltp) and a custom Kernel log parser.
     """,
     platforms='any',
     install_requires=['squad>=0.29', 'requests']
