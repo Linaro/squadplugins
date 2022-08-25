05ead52258065547420d63ad637102b0f6cf4f6d
Charles Oliveira <charles.oliveira@linaro.org>
2022-08-25 18:42:54 -0300

    ltp: reduce amount of db queries
    There was a test.save() on every new log line added to it,
    causing it to explode in case of many log lines matching
    test name.
    
    This also removes the need for regex.
    
    Signed-off-by: Charles Oliveira <charles.oliveira@linaro.org>

--- ltp/__init__.py
+++ ltp/__init__.py
@@ -1,24 +1,54 @@
 import logging
-import re
+from multiprocessing import Pool
 from squad.plugins import Plugin as BasePlugin
 
 
 logger = logging.getLogger()
+
+log_lines = None
+test_names = None
+
+def extract_test_log(test_id):
+    test_name = test_names[test_id]
+    logger.debug("Assigning LTP logs to %s" % test_name)
+    matches = []
+    for line in log_lines:
+        if not line.startswith(test_name):
+            continue
+
+        logger.debug("Found log line for test: %s" % test_name)
+        logger.debug(line)
+        matches.append(line)
+
+    logs = '\n'.join(matches) if len(matches) else None
+    return (test_id, logs)
 
 
 class LtpLogs(BasePlugin):
     name = "LTP Logs"
 
     def postprocess_testrun(self, testrun):
-        for test in testrun.tests.filter(result=False):
-            logger.debug("Assigning LTP logs to %s" % test.name)
-            regex = re.compile("^%s.*$" % re.escape(test.name), re.MULTILINE)
-            log_results = regex.findall(testrun.log_file)
-            for match in log_results:
-                logger.debug("Found log line for test: %s" % test.name)
-                logger.debug(match)
-                if test.log is None:
-                    test.log = match
-                else:
-                    test.log = test.log + "\r" + match
-                test.save()
+        global log_lines
+        global test_names
+
+        log_lines = testrun.log_file.split('\n')
+
+        tests = {}
+        test_names = {}
+        for t in testrun.tests.filter(result=False):
+            tests[t.id] = t
+            test_names[t.id] = t.name
+
+        results = None
+        with Pool() as pool:
+            results = pool.map(extract_test_log, test_names.keys())
+
+        for result in results:
+            test_id, log = result
+            if log is None:
+                continue
+            logger.debug(f'Saving test {t.id}')
+            if tests[test_id].log is None:
+                tests[test_id].log = ''
+            tests[test_id].log += log
+            tests[test_id].save()
--- test/test_ltp.py
+++ test/test_ltp.py
@@ -3,7 +3,6 @@ import unittest
 from unittest.mock import PropertyMock, Mock
 from ltp import LtpLogs
 
-
 TEST_LOG = """
 test_1: some log 1
 test_1: some log 2
@@ -24,65 +23,59 @@ class LtpLogsPluginTest(unittest.TestCase):
 
     def test_postprocess_testrun(self):
         test_1 = Mock()
-        test_1_name = PropertyMock(return_value="test_1")
-        type(test_1).name = test_1_name
-        test_1_log = PropertyMock()
-        type(test_1).log = test_1_log
+        test_1.id = 1
+        test_1.name = 'test_1'
+        test_1.log = 'the log 1\n'
+        test_1.save.return_value = None
 
         test_2 = Mock()
-        test_2_name = PropertyMock(return_value="test_2")
-        type(test_2).name = test_2_name
-        test_2_log = PropertyMock()
-        type(test_2).log = test_2_log
+        test_2.id = 2
+        test_2.name = 'test_2'
+        test_2.log = 'the log 2'
+        test_2.save.return_value = None
 
         test_3 = Mock()
-        test_3_name = PropertyMock(return_value="test_3")
-        type(test_3).name = test_3_name
-        test_3_log = PropertyMock()
-        type(test_3).log = test_3_log
+        test_3.id = 3
+        test_3.name = 'test_3'
+        test_3.log = None
+        test_3.save.return_value = None
 
         testrun = Mock()
-        testrun_log = PropertyMock(return_value=TEST_LOG)
-        type(testrun).log_file = testrun_log
+        testrun.log_file = TEST_LOG
 
         testrun.tests = Mock()
         testrun.tests.filter.return_value = [test_1, test_2, test_3]
 
         self.plugin.postprocess_testrun(testrun)
         testrun.tests.filter.assert_called_with(result=False)
-        testrun_log.assert_called_with()
 
-        test_1_name.assert_called_with()
-        test_1_log.assert_called()
+        self.assertEqual("the log 1\ntest_1: some log 1\ntest_1: some log 2\ntest_1: some log 3", test_1.log)
         test_1.save.assert_called_with()
 
-        # test_2 not present in the log
-        test_2_name.assert_called_with()
-        test_2_log.assert_not_called()
+        # Test 2 is not in the logs
+        self.assertEqual("the log 2", test_2.log)
         test_2.save.assert_not_called()
 
-        test_3_name.assert_called_with()
-        test_3_log.assert_called()
+        # Test 3 didn't have any logs
+        self.assertEqual("test_3: some log 1\ntest_3: some log 2\ntest_3: some log 3", test_3.log)
         test_3.save.assert_called_with()
 
     def test_tests_with_regex_characters(self):
         test = Mock()
-        test_name = PropertyMock(return_value="test[a-1]")
-        type(test).name = test_name
-        test_log = PropertyMock()
-        type(test).log = test_log
+        test.id = 1
+        test.name = "test[a-1]"
+        test.log = "the log\n"
 
         testrun = Mock()
-        testrun_log = PropertyMock(return_value=TEST_LOG)
-        type(testrun).log_file = testrun_log
+        testrun.log_file = TEST_LOG
 
         testrun.tests = Mock()
         testrun.tests.filter.return_value = [test]
 
         self.plugin.postprocess_testrun(testrun)
 
-        test_name.assert_called_with()
-        test_log.assert_called()
+        self.assertEqual("the log\ntest[a-1]: some log", test.log)
+
         test.save.assert_called_with()
 
 
